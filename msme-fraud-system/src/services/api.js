93bd2e3b14004edc282f23eda0ad8c498f6d588e
SAI RUTHVIK <23eg105k49@anurag.edu.in>
2026-03-31 09:47:23 +0530

    Update frontend API base URL to use environment variable

--- msme-fraud-system/src/services/api.js
+++ msme-fraud-system/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://host.docker.internal:8080', // backend URL
+    baseURL: import.meta.env.VITE_API_BASE_URL || "",
 });
 
 export default api;
